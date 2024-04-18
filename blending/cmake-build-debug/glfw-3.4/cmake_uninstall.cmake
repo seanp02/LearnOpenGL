2271ad1b86d699bb5acfb9f804ba97a20eb6fa14
seanp02 <seanp02@naver.com>
2024-04-18 13:26:11 +0900

    blending

---
+++ blending/cmake-build-debug/glfw-3.4/cmake_uninstall.cmake
@@ -0,0 +1,29 @@
+
+if (NOT EXISTS "/home/seanp/Desktop/git/LearnOpenGL/blending/cmake-build-debug/glfw-3.4/install_manifest.txt")
+    message(FATAL_ERROR "Cannot find install manifest: \"/home/seanp/Desktop/git/LearnOpenGL/blending/cmake-build-debug/glfw-3.4/install_manifest.txt\"")
+endif()
+
+file(READ "/home/seanp/Desktop/git/LearnOpenGL/blending/cmake-build-debug/glfw-3.4/install_manifest.txt" files)
+string(REGEX REPLACE "\n" ";" files "${files}")
+
+foreach (file ${files})
+  message(STATUS "Uninstalling \"$ENV{DESTDIR}${file}\"")
+  if (EXISTS "$ENV{DESTDIR}${file}")
+    exec_program("/home/seanp/Desktop/develop/clion/bin/cmake/linux/aarch64/bin/cmake" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
+                 OUTPUT_VARIABLE rm_out
+                 RETURN_VALUE rm_retval)
+    if (NOT "${rm_retval}" STREQUAL 0)
+      MESSAGE(FATAL_ERROR "Problem when removing \"$ENV{DESTDIR}${file}\"")
+    endif()
+  elseif (IS_SYMLINK "$ENV{DESTDIR}${file}")
+    EXEC_PROGRAM("/home/seanp/Desktop/develop/clion/bin/cmake/linux/aarch64/bin/cmake" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
+                 OUTPUT_VARIABLE rm_out
+                 RETURN_VALUE rm_retval)
+    if (NOT "${rm_retval}" STREQUAL 0)
+      message(FATAL_ERROR "Problem when removing symlink \"$ENV{DESTDIR}${file}\"")
+    endif()
+  else()
+    message(STATUS "File \"$ENV{DESTDIR}${file}\" does not exist.")
+  endif()
+endforeach()
+
